a851e3467e1cd6aa0e8c512034d0857a558c2d7c
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2021-12-10 11:55:07 +0530

    Header Updated

---
+++ Frontend/src/Components/header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from '../Assets/Logo/Bikerr-logo-white.png'
+import '../Styles/header.css'
+
+
+export default function header() {
+    return (
+        <div className='header'>
+            
+            <header>
+                
+                
+                    <img src= { Logo } alt="Bikerr-logo" />
+                
+
+                
+                <ul className="nav-links">
+
+                    <li> <a href="#"> Home </a> </li>
+                    <li> <a href="#"> About </a> </li>
+                    <li> <a href="#"> Bikes </a> </li>
+                    <li> <a href="#"> Login </a> </li>
+
+                    <button> Sign-up </button>
+
+                </ul>
+                
+
+            </header>
+
+        </div>
+    )
+}
